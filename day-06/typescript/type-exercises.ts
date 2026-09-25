f1217db84fe693e071e811aa7acb41671678311e
Shreya Thakare <shreyathakare79@gmail.com>
2026-09-25 22:19:18 +0530

    Delete day-06 directory

--- day-06/typescript/type-exercises.ts
+++
@@ -1,4 +0,0 @@
-import type { Employee } from './employee-types';
-const employees: Employee[] = [{id:1,name:'Asha Patil',email:'asha@example.com',department:'Engineering',position:'Developer',salary:65000}];
-const getAverageSalary = (items: Employee[]): number => items.reduce((sum,item)=>sum+item.salary,0)/items.length;
-console.log(getAverageSalary(employees));
